0e80aed5e8b7a9649b72c4593f830248d8ddd092
DiegoTheExplorar <133507153+DiegoTheExplorar@users.noreply.github.com>
2024-06-11 21:24:42 +0800

    Added in clean_noisy method to DDPPMPipeline in order to denoise noised ground truth

--- src/diffusers/pipelines/ddpm/pipeline_ddpm.py
+++ src/diffusers/pipelines/ddpm/pipeline_ddpm.py
@@ -199,3 +199,39 @@ class DDPMPipeline(DiffusionPipeline):
             image = self.numpy_to_pil(image)
 
         return {"sample": image}
+    
+    @torch.no_grad()
+    def clean_noisy(self, batch_size=1, generator=None, encoder_hidden_states=None, attention_mask=None, ground_truth_images=None, output_type="pil", **kwargs):
+        if "torch_device" in kwargs:
+            device = kwargs.pop("torch_device")
+            warnings.warn("`torch_device` is deprecated. Use `pipe.to(torch_device)` instead.")
+            # Set device as before (to be removed in 0.3.0)
+            if device is None:
+                device = "cuda" if torch.cuda.is_available() else "cpu"
+            self.to(device)
+        total_timesteps = 1000
+        if ground_truth_images is None:
+            image = torch.randn(
+            (batch_size, self.unet.in_channels, self.unet.sample_size[0], self.unet.sample_size[1]),
+            generator=generator,
+            )
+            image = image.to(self.device)
+        else:
+            self.scheduler.set_timesteps(total_timesteps)
+            image = self.scheduler.add_noise(ground_truth_images, torch.randn(ground_truth_images.shape).to(self.device), total_timesteps)
+        
+        for t in self.progress_bar(self.scheduler.timesteps):
+            # 1. predict noise model_output
+            model_output = self.unet(image, t, encoder_hidden_states, attention_mask)["sample"]
+
+            # 2. compute previous image: x_t -> t_t-1
+            result = self.scheduler.step_clean(model_output, t, image, generator=generator)#["prev_sample"]
+            image_clean = result['orig_sample']
+            image = result['prev_sample']
+
+            image_yield = (image / 2 + 0.5).clamp(0, 1)
+            image_yield = image_yield.cpu().permute(0, 2, 3, 1).numpy()
+
+            image_clean_yield = (image_clean / 2 + 0.5).clamp(0, 1)
+            image_clean_yield = image_clean_yield.cpu().permute(0, 2, 3, 1).numpy()
+            yield image_yield, image_clean_yield
